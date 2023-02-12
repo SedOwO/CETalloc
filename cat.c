e0ba7abf934a4542609cb22809deee522052451e
Skia <sairagav2003@gmail.com>
2023-02-12 19:05:38 +0530

    cleanup and free fix

--- cat.c
+++ cat.c
@@ -10,28 +10,20 @@ typedef struct Student
 	char name[20];
 	int rank;
 	char cat[2];
-	char clg[10];//in database this should hold NULL
+	char clg[10];
 	struct Student *lptr, *rptr;
 }node;
 typedef struct College
 {
 	char name[20];
-	node* pupils[25];//clg list will have student nodes assigned to them.
+	node* pupils[25];
 }college;
-
-//fn for inputs
-/*clg allocating(college c1, A1, B2, GM)
-{
-	count variables
-	return c1;
-}
-*/
 
 node* input(node *studentList){
         int n;
-        printf("Number of students: \n");
+        printf("Number of students(<=150):\n");
         scanf("%d",&n);
-        printf("\nenter details in the order: uid, name, rank, catageory:\n");
+        printf("\nEnter Student Details:\n UID   NAME   RANK   CATEGORY:\n");
         while(n--)
         {
                 node* new1=malloc(sizeof(node));
@@ -78,9 +70,10 @@ node* generatePQueue(node* catlist,node* studentlist,char ch)
 				goto TRAVERSE;
 			}
 			node *temp=catlist;
-			while(temp->rptr && ((temp->rptr)->rank)<new1->rank)//asscending priority
-				temp=temp->rptr; //find spot
-			//appedning
+			//finding ascending priority spot
+			while(temp->rptr && ((temp->rptr)->rank)<new1->rank)
+				temp=temp->rptr; 
+			//appending
 			new1->rptr=temp->rptr;
 			if(temp->rptr) //to avoid segufault
 				(temp->rtpr)->lptr=new1;
@@ -110,7 +103,7 @@ college collegeAllocator(college c, node** A1, node** B2, node** GM)
 			free(*A1);
 		else{
 			(*A1)=(*A1)->rptr;
-			free((*A1)->lptr);
+			free((*A1)->lptr));
 		}
 		count++;
 	}
@@ -127,7 +120,7 @@ college collegeAllocator(college c, node** A1, node** B2, node** GM)
 			free(*B2);
 		else{
 			(*B2)=(*B2)->rptr;
-			free((*B2)->lptr);
+			free((*B2)->lptr));
 		}
 		(c.pupils)[count]=dQed;
 		count++;
@@ -145,7 +138,7 @@ college collegeAllocator(college c, node** A1, node** B2, node** GM)
 			free(*GM);
 		else{
 			(*GM)=(*GM)->rptr;
-			free((*GM)->lptr);
+			free((*GM)->lptr));
 		}
 		(c.pupils)[count]=dQed;
 		count++;
@@ -155,7 +148,7 @@ college collegeAllocator(college c, node** A1, node** B2, node** GM)
 
 void main()
 {
-	node* studentlist; //to dynamically create and store students info.
+	node* studentlist;
 	node* A1, *B2, *GM;//prioity queues	
 	college rv, bsm, jss, msr, rns, pes;
 	A1=generatePQueue(A1,studentlist, 'A');
