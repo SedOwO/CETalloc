be77b35c94929031992c7713710625c4f631f2c4
Skia <sairagav2003@gmail.com>
2023-02-12 13:55:41 +0530

    defined structure

--- cat.c
+++ cat.c
@@ -1,10 +1,34 @@
-// ONLY INSERTION PART IS DONE (PRIORITY SLL)
-
+/* snode: student node
+cnode:clg node  
+*/
 #include<stdlib.h>
 #include<stdio.h>
-//cat bess
+#define A1SEATS 8 //maximim number of seats 
+#define B2SEATS 8 //for reservations in clgs
+#define GMSEATS 9 //for different categories
+typedef struct Student
+{
+	char name[20];
+	int rank;
+	char cat[2];
+	char clg[10];
+	int uid[5];
+	struct Student *lptr, *rptr;
+}snode;
+typedef struct College
+{
+	char name[20];
+	snode* pupils[25];//clg list will have student nodes assigned to them.
+	struct College *lptr, *rptr;
+}cnode;
 int main()
 {
+    snode* studentlist; //to dynamically create and store students info.
+    snode* A1, *B2, *GM;
+    cnode *rv, *rns, *pes, *bms, *msr, *jss;
+    
+    
+    
     int ch;
     int rank;
     for(;;)
